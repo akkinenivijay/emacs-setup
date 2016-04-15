cefa87dedab640e9852d2b6b570597289dfdfd03
Vijay Akkineni <vijay.akkineni@weather.com>
2016-04-15 01:59:59 -0400

    Cider mode changes

--- lisp/clojure-setup.el
+++ lisp/clojure-setup.el
@@ -2,30 +2,65 @@
 ;;; Code:
 ;;; Commentary:
 
+(use-package color-identifiers-mode
+  :ensure t
+  :config
+  (add-hook 'after-init-hook 'global-color-identifiers-mode)
+  )
+
 (use-package aggressive-indent :ensure t :defer t)
 
 (defun clj-refactor-hook ()
   (clj-refactor-mode 1)
   (yas-minor-mode 1)
-  (cljr-add-keybindings-with-prefix "C-c C-m")
+  (cljr-add-keybindings-with-prefix "C-c .")
   )
 
 (use-package clojure-mode
   :ensure t
   :init
-    (use-package clj-refactor :ensure t)
+    (use-package clj-refactor
+      :ensure t
+      :diminish clj-refactor-mode)
+    (defconst clojure--prettify-symbols-alist
+    '(("fn"   . ?λ)
+      ("__"   . ?⁈)))
   :config
+    (add-hook 'clojure-mode-hook 'global-prettify-symbols-mode)
     (add-hook 'clojure-mode-hook #'clj-refactor-hook)
     (add-hook 'clojure-mode-hook #'aggressive-indent-mode)
 )
+
+(defun cider-send-and-evaluate-sexp ()
+  "Sends the s-expression located before the point or the active
+  region to the REPL and evaluates it. Then the Clojure buffer is
+  activated as if nothing happened."
+  (interactive)
+  (if (not (region-active-p))
+      (cider-insert-last-sexp-in-repl)
+    (cider-insert-in-repl
+     (buffer-substring (region-beginning) (region-end)) nil))
+  (cider-switch-to-repl-buffer)
+  (cider-repl-closing-return)
+  (cider-switch-to-last-clojure-buffer)
+  (message ""))
 
 (use-package cider
   :ensure t
   :init
   (progn
-    (use-package cider-eval-sexp-fu
-      :ensure t
-      )
+    (use-package cider-eval-sexp-fu :ensure t)
+    (require 'cider-eval-sexp-fu)
+    (setq cider-repl-shortcut-dispatch-char ?\:)
+    (setq cider-repl-result-prefix "===>")
+    (setq nrepl-buffer-name-separator "-")
+    (setq nrepl-buffer-name-show-port t)
+    (setq cider-repl-wrap-history t)
+    (setq cider-repl-history-size 1000)
+    (setq nrepl-log-messages nil)
+    (setq cider-test-show-report-on-success t)
+    (setq cider-stacktrace-fill-column 80)
+    (setq cider-stacktrace-default-filters '(tooling dup))
     (add-hook 'cider-repl-mode-hook #'eldoc-mode)
     (add-hook 'cider-mode-hook #'eldoc-mode)
     (add-hook 'cider-repl-mode-hook #'company-mode)
@@ -33,18 +68,11 @@
     (add-hook 'cider-repl-mode-hook #'smartparens-strict-mode)
     (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
     (add-hook 'eval-expression-minibuffer-setup-hook #'eldoc-mode)
-    (require 'cider-eval-sexp-fu))
+    (bind-key "C-c C-v" 'cider-send-and-evaluate-sexp)
+    )
   :config
-  (setq cider-repl-shortcut-dispatch-char ?\:)
-  (setq cider-repl-result-prefix ";; => ")
-  (setq nrepl-buffer-name-separator "-")
-  (setq nrepl-buffer-name-show-port t)
-  (setq cider-repl-wrap-history t)
-  (setq cider-repl-history-size 1000)
-  (setq nrepl-log-messages nil)
-  (setq cider-test-show-report-on-success t)
-  (setq cider-stacktrace-fill-column 80)
-  (setq cider-stacktrace-default-filters '(tooling dup)))
+  (use-package slamhound)
+  )
 
 (provide 'clojure-setup)
 ;;;
