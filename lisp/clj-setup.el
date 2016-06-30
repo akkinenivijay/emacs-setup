ea3d2998f0b63c3f12991e3785bbd83f9574b6ae
Vijay Akkineni <vakkineni@liaison.com>
2016-06-30 18:05:37 -0400

    removed use-package

---
+++ lisp/clj-setup.el
@@ -0,0 +1,31 @@
+;;; package --- Clojure configuration
+;;; Code:
+;;; Commentary:
+
+;;clojure-mode setup
+(require 'clojure-mode-extra-font-locking)
+(add-hook 'clojure-mode-hook #'subword-mode)
+(add-hook 'clojure-mode-hook #'smartparens-strict-mode)
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
+
+;;clj-refactor setup
+(require 'clj-refactor)
+
+(defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+
+;;cider setup
+(setq cider-inject-dependencies-at-jack-in nil)
+(setq cljr-suppress-middleware-warnings t)
+(add-hook 'cider-repl-mode-hook #'eldoc-mode)
+(add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'company-mode)
+
+(provide 'clj-setup)
+;;;
